7ed62cf4398d43bc74d70a81fb14755657fc9231
Jack Tsai <jtsai0606@gmail.com>
2015-10-21 05:15:27 -0700

    When enemy vision collides with player the vision cone will change from yellow to red

--- Cyber Cube/Cyber_Cube/Enemy.cs
+++ Cyber Cube/Cyber_Cube/Enemy.cs
@@ -10,7 +10,7 @@ namespace CyberCube {
 	public class Enemy : Actor {
 
 		private Texture2D pixel;
-        private VertexPositionNormalTexture[] visionVertices;
+		private VertexPositionColorTexture[] visionVertices;
 		private int tickCounter;
         private Vector2 facingDirection;
 
@@ -29,10 +29,10 @@ namespace CyberCube {
                                    Color.White, Color.White, Color.White,
                                    Color.White, Color.White, Color.White } );
 
-            visionVertices = new VertexPositionNormalTexture[3];
-            visionVertices[0] = new VertexPositionNormalTexture(new Vector3(0, 0, 0), Vector3.UnitZ, new Vector2(0, 0));
-            visionVertices[1] = new VertexPositionNormalTexture(new Vector3(0.5f, 0, 0), Vector3.UnitZ, new Vector2(0.5f, 0));
-            visionVertices[2] = new VertexPositionNormalTexture(new Vector3(0.5f, 0.5f, 0), Vector3.UnitZ, new Vector2(0.5f, 0.5f));
+			visionVertices = new VertexPositionColorTexture[3];
+			visionVertices[0] = new VertexPositionColorTexture( new Vector3( 0, -0.01f, 0 ), Color.LightGoldenrodYellow, new Vector2( 0, 0 ) );
+			visionVertices[1] = new VertexPositionColorTexture( new Vector3( 0.5f, -0.01f, 0 ), Color.LightGoldenrodYellow, new Vector2( 0.5f, 0 ) );
+			visionVertices[2] = new VertexPositionColorTexture( new Vector3( 0.5f, 0.5f, 0 ), Color.LightGoldenrodYellow, new Vector2( 0.5f, 0.5f ) );
         }
 
 		protected override Vector3 TransformMovementTo3d( Vector2 vec2d )
@@ -50,40 +50,89 @@ namespace CyberCube {
 
 			mVelocity2d.Y -= 5f * timeDiff;
 			var xScale = FreeFall ? 0f : 8f;
+
 			Utils.FloatApproach( ref mVelocity2d.X, 0, xScale * timeDiff );
 
-			if (tickCounter < 160)
+			if (SeePlayer( Game.Player ))
 			{
-				mVelocity2d.X += xScale * timeDiff;
-                facingDirection = Vector2.UnitX;
-			}
-			else if (tickCounter >= 240 && tickCounter < 400)
-			{
-				mVelocity2d.X -= xScale * timeDiff;
-                facingDirection = -Vector2.UnitX;
-			}
+				xScale *= 1f;
+				visionVertices[0].Color = Color.DarkRed;
+				visionVertices[1].Color = Color.DarkRed;
+				visionVertices[2].Color = Color.DarkRed;
 
-			visionVertices[0] = new VertexPositionNormalTexture( WorldPosition, Vector3.UnitZ, new Vector2( 0, 0 ) );
-			if (facingDirection == Vector2.UnitX)
-			{
-				visionVertices[1] = new VertexPositionNormalTexture( WorldPosition + (new Vector3( 0.5f, 0, 0 )), Vector3.UnitZ, new Vector2( 0.5f, 0 ) );
-				visionVertices[2] = new VertexPositionNormalTexture( WorldPosition + (new Vector3( 0.5f, 0.5f, 0 )), Vector3.UnitZ, new Vector2( 0.5f, 0.5f ) );
+				//Vector2 playerDirection = Game.Player.ComputeFacePosition() - ComputeFacePosition();
+				//playerDirection.Normalize();
+				//playerDirection.Y = 0;
+				//facingDirection = playerDirection;
+				//tickCounter = 0;
 			}
 			else
 			{
-				visionVertices[1] = new VertexPositionNormalTexture( WorldPosition + (new Vector3( -0.5f, 0, 0 )), Vector3.UnitZ, new Vector2( 0.5f, 0 ) );
-				visionVertices[2] = new VertexPositionNormalTexture( WorldPosition + (new Vector3( -0.5f, 0.5f, 0 )), Vector3.UnitZ, new Vector2( 0.5f, 0.5f ) );
+				visionVertices[0].Color = Color.LightGoldenrodYellow;
+				visionVertices[1].Color = Color.LightGoldenrodYellow;
+				visionVertices[2].Color = Color.LightGoldenrodYellow;
 			}
+
+			if (tickCounter < 160)
+			{
+				facingDirection = Vector2.UnitX;
+			}
+			else if (tickCounter >= 240 && tickCounter < 400)
+			{
+				facingDirection = -Vector2.UnitX;
+			}
+			else
+			{
+				xScale = 0;
+			}
+			
+
+			visionVertices[0].Position = WorldPosition;
+			if (facingDirection == Vector2.UnitX)
+			{
+				visionVertices[1].Position = WorldPosition + (new Vector3( 0.5f, -0.01f, 0 ));
+				visionVertices[2].Position = WorldPosition + (new Vector3( 0.5f, 0.25f, 0 ));
+			}
+			else
+			{
+				xScale *= -1;
+				visionVertices[1].Position = WorldPosition + (new Vector3( -0.5f, -0.01f, 0 ));
+				visionVertices[2].Position = WorldPosition + (new Vector3( -0.5f, 0.25f, 0 ));
+			}
+			mVelocity2d.X += xScale * timeDiff;
+
 			tickCounter++;
 			tickCounter %= 480;
+
 
 			base.Update(gameTime);
 		}
 
-		public bool SeePlayer( Player player )
+		// we determine if we see the player by using the triangle vision and see if it contains the player 
+		private bool SeePlayer( Player player )
 		{
-			// Rectangle playerRectangle = new Rectangle( player.ComputeFacePosition(), player.Width(), player.Height() );
-			return false;
+			Vector2 playerPosition = player.ComputeFacePosition();
+			Vector2 pointA = Cube.ComputeFacePosition( visionVertices[0].Position, CubeFace );
+			Vector2 pointB = Cube.ComputeFacePosition( visionVertices[1].Position, CubeFace );
+			Vector2 pointC = Cube.ComputeFacePosition( visionVertices[2].Position, CubeFace );
+
+			// Alternative: Barycentric Technique
+
+
+			return PointInTriangle(playerPosition, pointA, pointB, pointC);
+		}
+
+		private bool SameSide(Vector2 point1, Vector2 point2, Vector2 pointA, Vector2 pointB)
+		{
+			
+			Vector3 cp1 = Vector3.Cross(new Vector3(pointB - pointA, 0), new Vector3(point1 - pointA, 0));
+			Vector3 cp2 = Vector3.Cross( new Vector3( pointB - pointA, 0 ), new Vector3( point2 - pointA, 0 ) );
+			return Vector3.Dot(cp1, cp2) >= 0;
+		}
+
+		private bool PointInTriangle(Vector2 p, Vector2 a, Vector2 b, Vector2 c)
+		{
+			return SameSide( p, a, b, c ) && SameSide( p, b, c, a ) && SameSide( p, c, a, b );
 		}
 
 		public override void Draw(GameTime gameTime) {
@@ -94,16 +143,21 @@ namespace CyberCube {
 				Cube.Effect.View,
 				Cube.Effect.World );
 
-            RasterizerState rasterizerState = new RasterizerState();
-            rasterizerState.CullMode = CullMode.None;
-            GraphicsDevice.RasterizerState = rasterizerState;
+			Cube.Effect.Alpha = 0.6f;
+			Cube.Effect.VertexColorEnabled = true;
+			Cube.Effect.LightingEnabled = false;
 
-			Cube.Effect.Alpha = 0.4f;
+			RasterizerState rasterizerState = new RasterizerState();
+			rasterizerState.CullMode = CullMode.None;
+			GraphicsDevice.RasterizerState = rasterizerState;
             foreach (EffectPass pass in Cube.Effect.CurrentTechnique.Passes)
             {
                 pass.Apply();
 				GraphicsDevice.DrawUserPrimitives( PrimitiveType.TriangleList, visionVertices, 0, 1 );
             }
+
+			Cube.Effect.LightingEnabled = true;
+			Cube.Effect.VertexColorEnabled = false;
 			Cube.Effect.Alpha = 1f;
 
 			mSpriteBatch.Begin();
