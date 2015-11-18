b219f033f4533347a89dab3a8a71005fc174ab1e
Lester Aquilario <laquilario@my.bcit.ca>
2015-11-18 10:33:46 -0800

    empty "end level" screen

---
+++ Cyber Cube/Cyber_Cube/Screens/EndLevelScreen.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+using System.IO;
+
+namespace CyberCube.Screens
+{
+    public class EndLevelScreen : GameScreen
+    {
+        private static SpriteFont sFont;
+
+        public static void LoadContent(ContentManager content)
+        {
+
+        }
+
+        public EndLevelScreen( CubeGame game )
+            :base (game)
+        {
+
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.Black);
+        }
+    }
+}
